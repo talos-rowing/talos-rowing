834886a091774f4a4e6b05274442af2221b569d8
Tal Shalif <tshalif@gmail.com>
2012-01-13 15:25:12 +0900

    misc: updated copyright notice

--- android/src/org/nargila/robostroke/android/app/GraphPanelDisplayManager.java
+++ android/src/org/nargila/robostroke/android/app/GraphPanelDisplayManager.java
@@ -16,24 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Talos-Rowing.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*
- * Copyright (c) 2011 Tal Shalif
- * 
- * This file is part of Talos-Rowing.
- * 
- * Talos-Rowing is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Talos-Rowing is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with Talos-Rowing.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.nargila.robostroke.android.app;
 
 import java.util.ArrayList;
