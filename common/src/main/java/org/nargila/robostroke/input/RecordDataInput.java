a44b9a5f61117f64f571b478f6338eb81ce8f755
Tal Shalif <tshalif@gmail.com>
2012-07-02 00:47:14 +0900

    refactor: new RecordDataInput base class

---
+++ common/src/main/java/org/nargila/robostroke/input/RecordDataInput.java
@@ -0,0 +1,33 @@
+package org.nargila.robostroke.input;
+
+import org.nargila.robostroke.RoboStrokeEventBus;
+
+public abstract class RecordDataInput extends SensorDataInputBase {
+
+	protected final RoboStrokeEventBus bus;
+
+	public RecordDataInput(RoboStrokeEventBus bus) {
+		this.bus = bus;
+	}
+
+	public void playRecord(DataRecord record) {
+		if (record.type.isReplayableEvent) {
+			switch (record.type) {
+			case GPS:
+				gpsDataSource.pushData(record.timestamp, record.data);		
+				break;
+			case ACCEL:
+				accelerometerDataSource.pushData(record.timestamp, record.data);
+				break;
+			case ORIENT:
+				orientationDataSource.pushData(record.timestamp, record.data);
+				break;
+			}
+	
+			if (record.type.isBusEvent && bus != null) {
+				bus.fireEvent(record);
+			}
+		}
+	}
+
+}
