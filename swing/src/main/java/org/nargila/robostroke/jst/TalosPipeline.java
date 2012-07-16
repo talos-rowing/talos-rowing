1c5eb14785557eadcde50cad3c058be600698e5c
Tal Shalif <tshalif@gmail.com>
2012-07-16 17:28:52 +0900

    misc: add copyright header note

--- swing/src/main/java/org/nargila/robostroke/jst/TalosPipeline.java
+++ swing/src/main/java/org/nargila/robostroke/jst/TalosPipeline.java
@@ -1,19 +1,20 @@
-/* Cortado - a video player java applet
- * Copyright (C) 2005 Fluendo S.L.
- *
- * This program is free software; you can redistribute it and/or modify
+/* (based on CortadoPipeline)
+ * Copyright (c) 2012 Tal Shalif
+ * 
+ * This file is part of Talos-Rowing.
+ * 
+ * Talos-Rowing is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * 
+ * Talos-Rowing is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ * along with Talos-Rowing.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.nargila.robostroke.jst;
